1e31424792e9082d032090a1afbbd9067fbdce75
Bobo Li <boboli@ENG.FLCNC.COM>
2019-12-31 17:49:58 +0800

    [Add] Add response in handle_connection

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,9 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n Helllo World!";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
+
