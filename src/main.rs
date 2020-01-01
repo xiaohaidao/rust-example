10d4388be7738e7f8ca891dc96b228afd2910356
Bob li <oxox0@qq.com>
2020-01-02 00:53:10 +0800

    [Add] Add thread pool

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::thread;
+use std::time::Duration;
+
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,8 +24,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (filename, status_line) = if buffer.starts_with(get) {
+        ("index.html", "HTTP/1.1 200 OK\r\n\r\n")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("index.html", "HTTP/1.1 200 OK\r\n\r\n")
     } else {
         ("404.html", "HTTP/1.1 404 NOT FOUND\r\n\r\n")
@@ -28,4 +39,3 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
-
